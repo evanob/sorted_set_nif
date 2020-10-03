1b261b532106b9461d3bab1c262ecea9d2d0b3de
Evan O'Brien <evan@cilabs.com>
2020-10-03 15:20:36 +0100

    Upgrade to rustler 0.21

--- mix.exs
+++ mix.exs
@@ -25,12 +25,12 @@ defmodule SortedSet.MixProject do
 
   defp deps do
     [
-      {:rustler, "~> 0.18"},
+      {:rustler, "~> 0.21"},
       {:ex_doc, "~> 0.19", only: [:dev], runtime: false},
       {:benchee, "~> 0.13", only: [:dev]},
       {:benchee_html, "~> 0.5", only: [:dev]},
       {:stream_data, "~> 0.4", only: [:test]},
-      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
